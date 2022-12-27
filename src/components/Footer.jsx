a6eb677ab9f25a6a358525b3c6d6170d71783263
david <davidrocerdeira@gmail.com>
2022-12-27 18:14:46 +0000

    Added footer.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full h-8 bg-background flex justify-center items-center'>
+        <div className='mb-10'> Coded and designed by David © 2022</div>
+    </div>
+  )
+}
+
+export default Footer
