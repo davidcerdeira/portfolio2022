f8f80fe496b8c9cb66eeda57524a8e82831063b5
david <davidrocerdeira@gmail.com>
2022-12-27 17:53:24 +0000

    Added skills to the about section and made it respnsive like I wanted.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 const About = () => {
+    const [windowSize, setWindowSize] = useState(getWindowSize());
+
+    useEffect(() => {
+        function handleWindowResize() {
+          setWindowSize(getWindowSize());
+        }
+    
+        window.addEventListener('resize', handleWindowResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleWindowResize);
+        };
+      }, []);
+
   return (
     <div name='about' className='w-full h-screen bg-background text-basic'>
         <div className='flex flex-col justify-center items-center w-full h-full'>
@@ -17,14 +31,33 @@ const About = () => {
                     <p>Hi. I'm David, nice to meet you. Take a look around.</p>
                 </div>
                 <div>
-                    <p className='mb-2'>I'm a front-end developer and I enjoy building beautiful, simple and aesthetic designs. I'm also very interested in video games and mobile apps.</p>
+                    <p className='mb-4'>I'm a front-end developer and I enjoy building beautiful, simple and aesthetic designs. I'm also very interested in video games and mobile apps.</p>
                     <p>If I'm not coding, you will find me learning languages, playing piano, working out or travelling around the world.</p>
+                    <div className='flex flex-row space-x-4 mt-4'>
+                        <p className='font-bold'>Skills:</p>
+                        <p className='bg-gray-200 px-1 rounded'>React</p>
+                        <p className='bg-gray-200 px-1 rounded'>JavaScript</p>
+                        <p className='bg-gray-200 px-1 rounded'>GraphQl</p>
+                        {windowSize.innerWidth >= 731  ? <p className='bg-gray-200 px-1 rounded'>C#</p> : ''}
+                        {windowSize.innerWidth >= 841 ? <p className='bg-gray-200 px-1 rounded'>Java</p> : ''}
+                    </div>
+                    <div className='flex flex-row space-x-4 mt-2'>
+                    {windowSize.innerWidth <= 731 ? <p className='bg-gray-200 px-1 rounded'>C#</p> : ''}
+                    {windowSize.innerWidth <= 841 ? <p className='bg-gray-200 px-1 rounded'>Java</p> : ''}
+                        <p className='bg-gray-200 px-1 rounded'>CSS</p>
+                        <p className='bg-gray-200 px-1 rounded'>HTML</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     
   )
+}
+
+function getWindowSize() {
+    const {innerWidth, innerHeight} = window;
+    return {innerWidth, innerHeight};
 }
 
 export default About
