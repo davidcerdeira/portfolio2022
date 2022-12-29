ebcd6aeb0ebd4a47729aa12afd342be5c8097f44
david <davidrocerdeira@gmail.com>
2022-12-29 18:09:49 +0000

    Made language dropdown responsive.

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
-import i18n from 'i18next';
+import i18n from "i18next";
 
-const Dropdown = () => {
+const Dropdown = ({ nav }) => {
   const dropdownRef = useRef();
 
   useEffect(() => {
     let handler = (e) => {
-      if(!dropdownRef.current.contains(e.target)){
+      if (!dropdownRef.current.contains(e.target)) {
         setShowOptions(false);
       }
-    }
+    };
 
-    document.addEventListener('mousedown', handler);
-  })
+    document.addEventListener("mousedown", handler);
+  });
 
   const [showOptions, setShowOptions] = useState(false);
   const [currentLang, setCurrentLang] = useState("EN");
@@ -23,36 +23,40 @@ const Dropdown = () => {
   const changeLanguage = (event, lang) => {
     setCurrentLang(event.target.value);
     setShowOptions(false);
-    console.log(lang)
+    console.log(lang);
     i18n.changeLanguage(lang);
   };
 
   return (
     <div>
-      <div class='relative inline-block text-left'>
+      <div class='relative inline-block text-left' ref={dropdownRef}>
         <div>
           <button
             onClick={handleClick}
             type='button'
-            class='inline-flex w-full justify-center px-4 pb-2 text-sm font-bold text-primary'
+            class={`inline-flex w-full justify-center px-4 pb-2 ${nav ? 'pt-0.5' : ''} text-base font-bold text-primary`}
             id='menu-button'
             aria-expanded='true'
             aria-haspopup='true'
           >
             {currentLang}
-            <svg
-              class='-mr-1 h-5 w-5 ml-1'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 20 20'
-              fill='currentColor'
-              aria-hidden='true'
-            >
-              <path
-                fill-rule='evenodd'
-                d='M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z'
-                clip-rule='evenodd'
-              />
-            </svg>
+            { !nav ? (
+              <svg
+                class='-mr-1 h-5 w-5 ml-1'
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+                aria-hidden='true'
+              >
+                <path
+                  fill-rule='evenodd'
+                  d='M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z'
+                  clip-rule='evenodd'
+                />
+              </svg>
+            ) : (
+              ''
+            )}
           </button>
         </div>
 
@@ -63,46 +67,45 @@ const Dropdown = () => {
             aria-orientation='vertical'
             aria-labelledby='menu-button'
             tabindex='-1'
-            ref={dropdownRef}
           >
             <div class='py-0' role='none'>
               <button
-                class='text-primary block py-2 text-sm hover:bg-red-300'
+                class='text-primary block py-2 text-sm h'
                 role='menuitem'
                 tabindex='-1'
                 id='menu-item-3'
                 value='PT'
-                onClick={event => changeLanguage(event, 'pt')}
+                onClick={(event) => changeLanguage(event, "pt")}
               >
                 PT
               </button>
               <button
-                class='text-primary block py-2 text-sm hover:bg-red-300'
+                class='text-primary block py-2 text-sm '
                 role='menuitem'
                 tabindex='-1'
                 id='menu-item-3'
                 value='EN'
-                onClick={event => changeLanguage(event, 'en')}
+                onClick={(event) => changeLanguage(event, "en")}
               >
                 EN
               </button>
               <button
-                class='text-primary block py-2 text-sm hover:bg-red-300'
+                class='text-primary block py-2 text-sm '
                 role='menuitem'
                 tabindex='-1'
                 id='menu-item-3'
                 value='KR'
-                onClick={event => changeLanguage(event, 'ko')}
+                onClick={(event) => changeLanguage(event, "ko")}
               >
                 KR
               </button>
               <button
-                class='text-primary block py-2 text-sm hover:bg-red-300'
+                class='text-primary block py-2 text-sm '
                 role='menuitem'
                 tabindex='-1'
                 id='menu-item-3'
                 value='CHI'
-                onClick={event => changeLanguage(event, 'zh')}
+                onClick={(event) => changeLanguage(event, "zh")}
               >
                 CHI
               </button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,96 +1,166 @@
-import React, { useState } from 'react'
-import { FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
-import { HiOutlineMail } from 'react-icons/hi'
-import { BsFillPersonLinesFill } from 'react-icons/bs'
-import { Link } from 'react-scroll'
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { Link } from "react-scroll";
 
-import { useTranslation } from 'react-i18next';
-import Dropdown from './Dropdown'
+import { useTranslation } from "react-i18next";
+import Dropdown from "./Dropdown";
 
 const Navbar = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const [nav, setNav] = useState(false);
-    const handleClick = () => setNav(!nav);
-    const onButtonClick = () => {
-        fetch('DavidCerdeiraResume.pdf').then(response => {
-            response.blob().then(blob => {
-                const fileURL = window.URL.createObjectURL(blob);
-                let alink = document.createElement('a');
-                alink.href = fileURL;
-                alink.download = 'DavidCerdeiraResume.pdf';
-                alink.click();
-            })
-        })
-    }
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+  const onButtonClick = () => {
+    fetch("DavidCerdeiraResume.pdf").then((response) => {
+      response.blob().then((blob) => {
+        const fileURL = window.URL.createObjectURL(blob);
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "DavidCerdeiraResume.pdf";
+        alink.click();
+      });
+    });
+  };
 
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-background text-basic'>
-        <div>
-             {/** <img src={Logo} alt="Logo Image" style={{width: '50px'}}></img>*/}
-             <p className='text-2xl font-bold'>DAVID CERDEIRA</p>
+      <div>
+        {/** <img src={Logo} alt="Logo Image" style={{width: '50px'}}></img>*/}
+        <p className='text-2xl font-bold'>DAVID CERDEIRA</p>
+      </div>
+
+      {/* menu */}
+      <ul className='hidden md:flex text-basic'>
+        <li>
+          <Link to='home' spy={true} smooth={true} duration={500}>
+            {t("NavBar.menuOption1")}
+          </Link>
+        </li>
+        <li>
+          <Link to='about' spy={true} smooth={true} duration={500}>
+            {t("NavBar.menuOption2")}
+          </Link>
+        </li>
+        <li>
+          <Link to='projects' spy={true} smooth={true} duration={500}>
+            {t("NavBar.menuOption3")}
+          </Link>
+        </li>
+        <li>
+          <Link to='contact' spy={true} smooth={true} duration={500}>
+            {t("NavBar.menuOption4")}
+          </Link>
+        </li>
+        <Dropdown />
+      </ul>
+
+      {/* hamburger */}
+      <div className='md:hidden z-10'>
+        <div className='absolute top-7 right-10'>
+          {!nav ? <Dropdown nav={!nav} /> : ''}
         </div>
+        <div onClick={handleClick}>
+          {!nav ? <FaBars /> : <FaTimes />}
+        </div>
+      </div>
 
-        
+      {/* mobile menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-background flex flex-col justify-center items-center"
+        }
+      >
+        <li className='py-6 text-4xl'>
+          <Link
+            onClick={handleClick}
+            to='home'
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            {t("NavBar.menuOption1")}
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link
+            onClick={handleClick}
+            to='about'
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            {t("NavBar.menuOption2")}
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link
+            onClick={handleClick}
+            to='projects'
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            {t("NavBar.menuOption3")}
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link
+            onClick={handleClick}
+            to='contact'
+            spy={true}
+            smooth={true}
+            duration={500}
+          >
+            {t("NavBar.menuOption4")}
+          </Link>
+        </li>
+      </ul>
 
-        {/* menu */}
-        <ul className='hidden md:flex text-basic'>
-            <li>
-                <Link to="home" spy={true} smooth={true} duration={500}>{t('NavBar.menuOption1')}</Link>
-            </li>
-            <li>
-                <Link to="about" spy={true} smooth={true} duration={500}>{t('NavBar.menuOption2')}</Link>
-            </li>
-            <li>
-                <Link to="projects" spy={true} smooth={true} duration={500}>{t('NavBar.menuOption3')}</Link>
-            </li>
-            <li>
-                <Link to="contact" spy={true} smooth={true} duration={500}>{t('NavBar.menuOption4')}</Link>
-            </li>
-            <Dropdown/>
+      {/* social icons */}
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-github'>
+            <a
+              className='flex justify-between items-center w-full text-white'
+              href='https://github.com/realhackerdavid'
+              target='_blank'
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-email'>
+            <a
+              className='flex justify-between items-center w-full text-white'
+              href='mailto:davidrocerdeira@gmail.com'
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-resume'>
+            <a
+              className='flex justify-between items-center w-full text-white'
+              onClick={onButtonClick}
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue'>
+            <a
+              className='flex justify-between items-center w-full text-white'
+              href='https://www.linkedin.com/in/david-cerdeira/'
+              target='_blank'
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
         </ul>
-
-        {/* hamburger */}
-        <div onClick={handleClick} className='md:hidden z-10'>
-            {!nav ? <FaBars/> : <FaTimes/>}
-        </div>
-
-        {/* mobile menu */}
-        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-background flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4xl'><Link onClick={handleClick} to="home" spy={true} smooth={true} duration={500}>{t('NavBar.menuOption1')}</Link></li>
-            <li className='py-6 text-4xl'><Link onClick={handleClick} to="about" spy={true} smooth={true} duration={500}>{t('NavBar.menuOption2')}</Link></li>
-            <li className='py-6 text-4xl'><Link onClick={handleClick} to="projects" spy={true} smooth={true} duration={500}>{t('NavBar.menuOption3')}</Link></li>
-            <li className='py-6 text-4xl'><Link onClick={handleClick} to="contact" spy={true} smooth={true} duration={500}>{t('NavBar.menuOption4')}</Link></li>
-        </ul>
-
-        {/* social icons */}
-        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-            <ul>
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-github'>
-                    <a className='flex justify-between items-center w-full text-white' href="https://github.com/realhackerdavid" target="_blank">
-                        Github <FaGithub size={30}/>
-                    </a>
-                </li>
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-email'>
-                    <a className='flex justify-between items-center w-full text-white' href="mailto:davidrocerdeira@gmail.com">
-                        Email <HiOutlineMail size={30}/>
-                    </a>
-                </li>
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-resume'>
-                    <a className='flex justify-between items-center w-full text-white' onClick={onButtonClick}>
-                        Resume <BsFillPersonLinesFill size={30}/>
-                    </a>
-                </li>
-                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue'>
-                    <a className='flex justify-between items-center w-full text-white' href="https://www.linkedin.com/in/david-cerdeira/" target="_blank">
-                        LinkedIn <FaLinkedin size={30}/>
-                    </a>
-                </li>
-            </ul>
-        </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
