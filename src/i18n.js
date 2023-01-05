3d7c43d12627179d1f2311e74d90c77f08282baa
David Cerdeira <59337237+realhackerdavid@users.noreply.github.com>
2023-01-05 20:21:54 +0000

    Still trying to fix translations not working on gh pages.

--- src/i18n.js
+++ src/i18n.js
@@ -3,15 +3,16 @@ import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 
 i18n
-  .use(Backend)
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
-    lng: "en", // if you're using a language detector, do not define the lng option
+    debug: isDevelopment,
     fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
     backend: {
       loadPath: "/portfolio2022/locales/{{lng}}/{{ns}}.json",
-    },
-    interpolation: {
-      escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
     },
   });
