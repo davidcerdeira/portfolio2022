5f634ee1f4b6f0d15574606631a4d56f16a337e3
David Cerdeira <59337237+realhackerdavid@users.noreply.github.com>
2023-01-05 20:13:09 +0000

    Fixed some warnings with the dropdown. Tried fixing translations not showing on gh pages.

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -23,18 +23,17 @@ const Dropdown = ({ nav }) => {
   const changeLanguage = (event, lang) => {
     setCurrentLang(event.target.value);
     setShowOptions(false);
-    console.log(lang);
     i18n.changeLanguage(lang);
   };
 
   return (
     <div>
-      <div class='relative inline-block text-left' ref={dropdownRef}>
+      <div className='relative inline-block text-left' ref={dropdownRef}>
         <div>
           <button
             onClick={handleClick}
             type='button'
-            class={`inline-flex w-full justify-center px-4 pb-2 ${
+            className={`inline-flex w-full justify-center px-4 pb-2 ${
               nav ? "pt-0.5" : ""
             } text-base font-bold text-primary`}
             id='menu-button'
@@ -44,16 +43,16 @@ const Dropdown = ({ nav }) => {
             {currentLang}
             {!nav ? (
               <svg
-                class='-mr-1 h-5 w-5 ml-1'
+                className='-mr-1 h-5 w-5 ml-1'
                 xmlns='http://www.w3.org/2000/svg'
                 viewBox='0 0 20 20'
                 fill='currentColor'
                 aria-hidden='true'
               >
                 <path
-                  fill-rule='evenodd'
+                  fillRule='evenodd'
                   d='M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z'
-                  clip-rule='evenodd'
+                  clipRule='evenodd'
                 />
               </svg>
             ) : (
@@ -64,18 +63,18 @@ const Dropdown = ({ nav }) => {
 
         {showOptions ? (
           <div
-            class='absolute z-10 ml-4 mt-0 rounded-md bg-background focus:outline-none'
+            className='absolute z-10 ml-4 mt-0 rounded-md bg-background focus:outline-none'
             role='menu'
             aria-orientation='vertical'
             aria-labelledby='menu-button'
-            tabindex='-1'
+            tabIndex='-1'
           >
-            <div class='py-0' role='none'>
+            <div className='py-0' role='none'>
               {currentLang !== "EN" ? (
                 <button
-                  class='text-primary block py-2 text-sm'
+                  className='text-primary block py-2 text-sm'
                   role='menuitem'
-                  tabindex='-1'
+                  tabIndex='-1'
                   id='menu-item-3'
                   value='EN'
                   onClick={(event) => changeLanguage(event, "en")}
@@ -87,9 +86,9 @@ const Dropdown = ({ nav }) => {
               )}
               {currentLang !== "KR" ? (
                 <button
-                  class='text-primary block py-2 text-sm'
+                  className='text-primary block py-2 text-sm'
                   role='menuitem'
-                  tabindex='-1'
+                  tabIndex='-1'
                   id='menu-item-3'
                   value='KR'
                   onClick={(event) => changeLanguage(event, "ko")}
@@ -101,9 +100,9 @@ const Dropdown = ({ nav }) => {
               )}
               {currentLang !== "CHI" ? (
                 <button
-                  class='text-primary block py-2 text-sm'
+                  className='text-primary block py-2 text-sm'
                   role='menuitem'
-                  tabindex='-1'
+                  tabIndex='-1'
                   id='menu-item-3'
                   value='CHI'
                   onClick={(event) => changeLanguage(event, "zh")}
--- src/i18n.js
+++ src/i18n.js
@@ -1,30 +1,16 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
-
-import translationEN from "../public/locales/en/translation.json";
-import translationKO from "../public/locales/ko/translation.json";
-import translationZH from "../public/locales/zh/translation.json";
-
-const resources = {
- en: {
-   translation: translationEN,
- },
- ko: {
-   translation: translationKO,
- },
- zh: {
-  translation: translationZH,
-},
-};
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
 
 i18n
   .use(Backend)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    lng: 'en', // if you're using a language detector, do not define the lng option
-    fallbackLng: 'en',
-    resources,
+    lng: "en", // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
+    backend: {
+      loadPath: "/portfolio2022/locales/{{lng}}/{{ns}}.json",
+    },
     interpolation: {
       escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
     },
