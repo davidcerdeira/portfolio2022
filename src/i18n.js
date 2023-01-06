0ab2a73aa23e163c5c1d9d7600f2b128fdd85ca8
David Cerdeira <59337237+realhackerdavid@users.noreply.github.com>
2023-01-06 00:01:32 +0000

    Fixed translations not working on gh pages.

--- src/i18n.js
+++ src/i18n.js
@@ -1,12 +1,13 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
-  .use(HttpApi)
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    debug: isDevelopment,
     fallbackLng: "en",
     interpolation: {
       escapeValue: false,
